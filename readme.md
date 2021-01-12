a9d950476828c3891601f4b97d222fb53c717540
Sean Hsieh <sean.hsieh@garmin.com>
2021-01-12 08:45:44 +0800

    init
    Change-Id: Ia037a27a3c5a33d7886957015ec2d9abc0561ec0

---
+++ readme.md
@@ -0,0 +1,100 @@
+# Gauge Component for React
+A library that provides basic gauge styles and widgets to create gauges efficiently for React projects.
+
+## Installation and usage
+The easiest way to use react-ui-gauge-component is to install it from npm.
+
+```s
+npm i react-ui-gauge-component
+```
+
+Then use it in your app
+
+```javascript
+import React from 'react';
+import withGauge, {HeartBeat} from 'react-ui-gauge-component';
+
+const Gauge1 = withGauge({
+    WrappedComponent: HeartBeat,
+    colorArray: ["#eb001e", "#f35120", "#3aff20", "#53a5fd", "#aaaaaa"],
+    fields: [{title: "Gauge 1", value: "100"}]
+});
+
+const Gauge2 = withGauge({
+    WrappedComponent: HeartBeat,
+    colorArray: ["#eb001e", "#000", "#3aff20", "#53a5fd", "#aaaaaa"],
+    fields: [{title: "Gauge 1", value: "100"},
+    {title: "Gauge 2", value: "100"}]
+});
+
+const Gauge3 = withGauge({
+    WrappedComponent: HeartBeat,
+    colorArray: ["#eb001e", "#f35120", "#3aff20", "#53a5fd", "#aaaaaa"],
+    fields: [
+        {title: "Gauge 1", value: "100"},
+        {title: "Gauge 2", value: "100"},
+        {title: "Gauge 3", value: "100"}]
+});
+
+const Gauge4 = withGauge({
+    WrappedComponent: HeartBeat,
+    colorArray: ["#eb001e", "#f35120", "#3aff20", "#53a5fd", "#aaaaaa"],
+    fields: [
+        {title: "Gauge 1", value: "100"},
+        {title: "Gauge 2", value: "100"},
+        {title: "Gauge 3", value: "100"},
+        {title: "Gauge 4", value: "100"}]
+});
+
+const DashboardExample = () => {
+    return (
+        <Gauge1/>
+        <Gauge2/>
+        <Gauge3/>
+        <Gauge4/>
+    )
+}
+
+export default DashboardExample;
+
+```
+
+## Props
+* WrappedComponent -  Put a react component as a widget to compose a new gauge. The default widget is the heartbeat icon.
+* colorArray - Put an array of color for the gauge border.
+
+```javascript
+colorArray: ["#eb001e", "#f35120", "#3aff20", "#53a5fd", "#aaaaaa"]
+```
+
+* fields - Put an array of key, value pairs for the gauge's contents.
+
+```javascript
+fields: [
+        {title: "Gauge 1", value: "100"},
+        {title: "Gauge 2", value: "100"},
+        {title: "Gauge 3", value: "100"},
+        {title: "Gauge 4", value: "100"}]
+```
+
+## License
+MIT Licensed. Copyright (c) Jen-Hsuan Hsieh 2021.
+
+## About
+* [Author](https://jenhsuan.github.io/ALayman/profile.html)
+* [100DaysOfCode](https://dev.to/jenhsuan)
+* [Medium](https://medium.com/a-layman)
+
+## Links
+* [Change log]()
+
+## Refernces
+* [[Guide] 發佈 npm 套件 - 從手動到自動（0）：專案與套件建置篇](https://pjchender.github.io/2020/02/02/guide-%E7%99%BC%E4%BD%88-npm-%E5%A5%97%E4%BB%B6-%E5%BE%9E%E6%89%8B%E5%8B%95%E5%88%B0%E8%87%AA%E5%8B%95%EF%BC%880%EF%BC%89%EF%BC%9A%E5%B0%88%E6%A1%88%E8%88%87%E5%A5%97%E4%BB%B6%E5%BB%BA%E7%BD%AE%E7%AF%87/)
+* [發佈 npm 套件 - 從手動到自動（2）：手動 publish 篇](https://pjchender.blogspot.com/2020/02/guide-npm-2.html)
+* [React.js编译](https://chenshenhai.github.io/rollupjs-note/note/chapter04/01.html)
+* [如何使用Rollup打包样式文件并添加LiveReload](https://www.w3cplus.com/javascript/learn-rollup-css.html)
+* [Babel helpers are bundled into the output file](https://github.com/rollup/rollup-plugin-babel/issues/322)
+* [rollup使用过程中遇到的各种问题](https://xudany.github.io/rollup/2019/08/13/rollup%E4%BD%BF%E7%94%A8%E8%BF%87%E7%A8%8B%E4%B8%AD%E9%81%87%E5%88%B0%E7%9A%84%E5%90%84%E7%A7%8D%E9%97%AE%E9%A2%98/)
+* [rollup打包js的注意點-haorooms部落格分享](https://www.itread01.com/content/1542163957.html)
+* [The complete guide to publishing a React package to npm](https://blog.logrocket.com/the-complete-guide-to-publishing-a-react-package-to-npm/)
+* [How does React decide to re-render a component?](https://lucybain.com/blog/2017/react-js-when-to-rerender/)
---
+++ src/components/SimpleGauge.js
@@ -0,0 +1,216 @@
+import React, { Fragment } from 'react';
+
+const HeartBeat = () => {
+    return (
+        <g>
+            <path fill="tomato" d="M92.71,7.27L92.71,7.27c-9.71-9.69-25.46-9.69-35.18,0L50,14.79l-7.54-7.52C32.75-2.42,17-2.42,7.29,7.27v0 c-9.71,9.69-9.71,25.41,0,35.1L50,85l42.71-42.63C102.43,32.68,102.43,16.96,92.71,7.27z"></path>
+            <animateTransform
+                 attributeName="transform"
+                 type="scale"
+                 values="0.8; 1; 0.9; 1; 1; 0.8;"
+                 dur="1s"
+                 repeatCount="indefinite">
+                </animateTransform>
+        </g>
+    )
+}
+
+const withGauge = ({ WrappedComponent = HeartBeat, colorArray = [], fields = [{title: "Gauge 1", value: "100"}]}) => () => {
+    const fieldsNumber = fields.length;
+    if (fieldsNumber > 4) {
+        fields = fields.slice(0 , 4);
+    }
+
+    return (
+        <svg
+            viewBox="0 0 120 120"
+            className="chart-gauge"
+            fill="none"
+            strokeWidth="2">
+            <circle 
+            strokeDasharray= "200"
+            strokeDashoffset= "220"
+            stroke = {colorArray && colorArray[0] ? colorArray[0] : "#eb001e"} 
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            <circle 
+            strokeDasharray= "200"
+            strokeDashoffset= "270"
+            stroke = {colorArray && colorArray[1] ? colorArray[1] : "#f35120"} 
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            <circle 
+            strokeDasharray= "200"
+            strokeDashoffset= "310"
+            stroke = {colorArray && colorArray[2] ? colorArray[2] : "#3aff20"} 
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            <circle 
+            strokeDasharray= "200"
+            strokeDashoffset= "350"
+            stroke = {colorArray && colorArray[3] ? colorArray[3] : "#53a5fd"} 
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            <circle 
+            strokeDasharray= "200"
+            strokeDashoffset= "390"
+            stroke = {colorArray && colorArray[4] ? colorArray[4] :"#aaaaaa"} 
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            <circle 
+            strokeDasharray = "200"
+            strokeDashoffset = "420"
+            stroke = "#fff"
+            r = "57.32"
+            cx = "60"
+            cy = "60"
+            strokeWidth = "5"/>
+            {fieldsNumber === 1 && (
+                <Fragment>
+                    <text className="gauge-text" x="80" y="35" dx="-20" dy="1em" fill="black" fontSize='0.8rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].title}
+                    </text>
+                    <text className="gauge-text" x="80" y="55" dx="-20" dy="1em" fill="black" fontSize='1.1rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].value}
+                    </text>
+                    <svg width="15" height="15" viewBox="0 0 100 100" x="52" y="90">
+                        <WrappedComponent/>
+                    </svg>
+                </Fragment>
+            )}
+            {fieldsNumber === 2 && (
+                <Fragment>
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="10"
+                        y1="60"
+                        x2="110"
+                        y2="60" />
+                        <text className="gauge-text" x="80" y="20" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                            {fields[0].title}
+                        </text>
+                        <text className="gauge-text" x="80" y="38" dx="-20" dy="1em" fill="black" fontSize='0.8rem' textAnchor='middle' fontFamily='arial'>
+                            {fields[0].value}
+                        </text>
+                        <text className="gauge-text" x="80" y="70" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                            {fields[1].title}
+                        </text>
+                        <text className="gauge-text" x="80" y="88" dx="-20" dy="1em" fill="black" fontSize='0.8rem' textAnchor='middle' fontFamily='arial'>
+                            {fields[1].value}
+                        </text>
+                </Fragment>
+            )}
+            {fieldsNumber === 3 && (
+                <Fragment>
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="10"
+                        y1="60"
+                        x2="110"
+                        y2="60" />
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="60"
+                        y1="60"
+                        x2="60"
+                        y2="110" />
+                    <text className="gauge-text" x="80" y="15" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].title}
+                    </text>
+                    <text className="gauge-text" x="80" y="30" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].value}
+                    </text>
+                    <text className="gauge-text" x="57" y="65" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[1].title}
+                    </text>
+                    <text className="gauge-text" x="57" y="82" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[1].value}
+                    </text>
+                    <text className="gauge-text" x="105" y="65" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[2].title}
+                    </text>
+                    <text className="gauge-text" x="105" y="82" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[2].value}
+                    </text>
+                </Fragment>
+            )}
+            {fieldsNumber === 4 && (
+                <Fragment>
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="15"
+                        y1="42"
+                        x2="105"
+                        y2="42" />
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="60"
+                        y1="42"
+                        x2="60"
+                        y2="77" />
+                    <line
+                        stroke="red"
+                        strokeLinecap="round"
+                        strokeWidth="1"
+                        x1="15"
+                        y1="77"
+                        x2="105"
+                        y2="77" />
+                    <text className="gauge-text" x="80" y="10" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].title}
+                    </text>
+                    <text className="gauge-text" x="80" y="20" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[0].value}
+                    </text>
+                    <text className="gauge-text" x="55" y="48" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[1].title}
+                    </text>
+                    <text className="gauge-text" x="55" y="58" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[1].value}
+                    </text>
+                    <text className="gauge-text" x="105" y="48" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[2].title}
+                    </text>
+                    <text className="gauge-text" x="105" y="58" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[1].value}
+                    </text>
+                    <text className="gauge-text" x="80" y="80" dx="-20" dy="1em" fill="black" fontSize='0.5rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[3].title}
+                    </text>
+                    <text className="gauge-text" x="80" y="90" dx="-20" dy="1em" fill="black" fontSize='0.7rem' textAnchor='middle' fontFamily='arial'>
+                        {fields[3].value}
+                    </text>
+                    <svg width="10" height="10" viewBox="0 0 100 100" x="38" y="93">
+                        <WrappedComponent/>
+                    </svg>
+                </Fragment>
+            )}
+        </svg>
+    )
+}
+
+export {
+    HeartBeat
+};
+
+export default withGauge;
